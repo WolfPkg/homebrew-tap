5a7b6872d45b86cbbdeee10c6fea3c64f43f1b8e
Tino Didriksen <mail@tinodidriksen.com>
2024-02-02 12:11:20 +0000

    lttoolbox: add 3.7.6+g611-071c27e3 bottle.

--- Formula/lttoolbox.rb
+++ Formula/lttoolbox.rb
@@ -8,6 +8,12 @@ class Lttoolbox < Formula
 
   head "https://github.com/apertium/lttoolbox.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/WolfPkg/homebrew-tap/releases/download/lttoolbox-3.7.6+g611-071c27e3"
+    sha256 cellar: :any,                 ventura:      "86539ff5591ce5b2d723c32dde299d354a6d48c32f7a6dd2c05aed5348a620b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ed53da140e40b160013604dabf9303a660f529d2849d46c9e8a41d2c5dd8a82a"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
